c81f7e24a60a7f817daa7707d21259eaaf0d5b1a
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-11-26 15:32:31 -0600

    Made tailwind work

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ const Nav = ()=> {
 
   return(
     <div className='z-50 flex flex-col fixed'>
-      <nav className="bg-grey2 flex justify-between fixed w-full text-white">
+      <nav className="bg-black2 flex justify-between fixed w-full text-white">
         Navbar
       </nav>
     </div>
