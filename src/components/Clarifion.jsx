2b1b51ac07009dc79236c9740ebd515a7f51e29a
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-11-26 22:55:55 -0600

    Add multiple color text, add clarifion image with offer

--- src/components/Clarifion.jsx
+++ src/components/Clarifion.jsx
@@ -4,6 +4,9 @@ import norton from '@/assets/norton-antivirus.svg';
 import checkedstep from '@/assets/checked-step.svg';
 import currentstep from '@/assets/current-step.svg';
 import futurestep from '@/assets/future-step.svg';
+import star from '@/assets/star-filled.svg';
+import smallcircle from '@/assets/small-circle.svg';
+import clarifion from '@/assets/clarifion-nobg.svg';
 
 const Clarifion = ()=> {
   return(
@@ -35,6 +38,36 @@ const Clarifion = ()=> {
         <div className='flex flex-col items-center text-base text-center'>
           <img src={futurestep} alt="step" />
           Confirmation
+        </div>
+      </div>
+      <div className='text-3xl text-center py-6'>
+        <span className="text-blue2">ONE TIME ONLY </span>
+        <span className="text-black">Special Price For 6 Extra Clarifion For Only </span>
+        <span className="text-blue2">$14 Each </span>
+        <span className="text-black">($84.00 Total!)</span>
+      </div>
+      <div className='w-full'>
+        <div className='flex justify-between w-full'>
+          <img className='bg-blue2 rounded-md' src={clarifion} alt="clarifionIMG" />
+          <div>
+            <div className='flex justify-between'>
+              Clarifion Air Ionizer
+              <div className='flex justify-between items-center'>
+                <div className='text-sm'>$180</div>
+                <div className='text-[14px]'>$84</div>
+              </div>
+            </div>
+            <div className='flex'>
+              <img src={star} alt="star" />
+              <img src={star} alt="star" />
+              <img src={star} alt="star" />
+              <img src={star} alt="star" />
+              <img src={star} alt="star" />
+            </div>
+            <div>
+              <img src={smallcircle} alt="O" />
+            </div>
+          </div>
         </div>
       </div>
       
